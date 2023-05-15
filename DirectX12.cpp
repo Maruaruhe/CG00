99460efccc62d10af829baf31f8aad6dcf1ba3c3
k022g0101@g.neec.ac.jp <k022g0101@g.neec.ac.jp>
2023-05-15 17:14:18 +0900

    a

--- DirectX12.cpp
+++ DirectX12.cpp
@@ -1,4 +1,7 @@
 #include "DirectX12.h"
+#include <string>
+#include <format>
+
 DirectX12::DirectX12() {
 	
 }
@@ -10,6 +13,21 @@ void DirectX12::Init() {
 }
 void DirectX12::Adapter() {
 	IDXGIAdapter4* useAdapter = nullptr;
+
+	for (UINT i = 0; dxgiFactory->EnumAdapterByGpuPreference(i, DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(&useAdapter)) != DXGI_ERROR_NOT_FOUND; ++i) {
+		//GetAdapterInfo
+		DXGI_ADAPTER_DESC3 adapterDesc{};
+		hr = useAdapter->GetDesc3(&adapterDesc);
+		assert(SUCCEEDED(hr));
+		//ソフトウェアでなければok
+		if (!(adapterDesc.Flags & DXGI_ADAPTER_FLAG3_SOFTWARE)) {
+			//logに出力
+			Log(ConvertString(std::format(L"Use Adapter:{}\n", adapterDesc.Description)));
+			break;
+		}
+		useAdapter = nullptr;
+	}
+	assert(useAdapter != nullptr);
 }
 void DirectX12::D3D12Device() {
 
