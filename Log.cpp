9fd419dcb98af9e3f308d7782cb63045a642742a
k022g0101@g.neec.ac.jp <k022g0101@g.neec.ac.jp>
2023-05-15 17:26:12 +0900

    a

--- Log.cpp
+++ Log.cpp
@@ -1,8 +1,12 @@
 #include "Log.h"
 #include <d3d12.h>
+#include <dxgi1_6.h>
+#include <cassert>
 
 #pragma comment(lib,"d3d12.lib")
+#pragma comment(lib,"dxgi.lib")
 
 void Log::LogText(const std::string& message) {
 	OutputDebugStringA(message.c_str());
 }
+
