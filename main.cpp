8a39451128c0a9309b532b73fa29b91039416fc4
k022g0101@g.neec.ac.jp <k022g0101@g.neec.ac.jp>
2023-07-21 16:26:05 +0900

    a

--- main.cpp
+++ main.cpp
@@ -9,19 +9,17 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	
 	mainRoop->Initialize(windowsAPI, directX12);
 
-	mainRoop->VariableInit();
-
 	//いろいろ
 
 	//三角形の座標
 	//一つ目
-	Vector2 LeftBottom1 = { -1.0f,-0.5f };
-	Vector2 MiddleTop1 = { -0.5f,0.5f };
-	Vector2 RightBottom1 = { -0.25f,-0.5f };
-	//二つ目
-	Vector2 LeftBottom2 = { 0.25f,-0.25f };
-	Vector2 MiddleTop2 = { 0.3f,0.3f };
-	Vector2 RightBottom2 = { 0.4f,-0.1f };
+	//Vector2 LeftBottom1 = { -1.0f,-0.5f };
+	//Vector2 MiddleTop1 = { -0.5f,0.5f };
+	//Vector2 RightBottom1 = { -0.25f,-0.5f };
+	////二つ目
+	//Vector2 LeftBottom2 = { 0.25f,-0.25f };
+	//Vector2 MiddleTop2 = { 0.3f,0.3f };
+	//Vector2 RightBottom2 = { 0.4f,-0.1f };
 
 	//初期化
 
