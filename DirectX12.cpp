ca8cc68d9cc5bde2e5aaf180176af976a4e84e5e
aruma <k022g0101@g.neec.ac.jp>
2023-07-31 06:33:03 +0900

    s

--- DirectX12.cpp
+++ DirectX12.cpp
@@ -29,7 +29,12 @@ void DirectX12::InitializeDirectX12(WindowsAPI* winAPI) {
 	ImGui::CreateContext();
 	ImGui::StyleColorsDark();
 	ImGui_ImplWin32_Init(winAPI_->GetHwnd());
-	ImGui_ImplDX12_Init(device, swapChainDesc.BufferCount, rtvDesc.Format, srvDescriptorHeap, srvDescriptorHeap->GetCPUDescriptorHandleForHeapStart(), srvDescriptorHeap->GetGPUDescriptorHandleForHeapStart());
+	ImGui_ImplDX12_Init(device, 
+		swapChainDesc.BufferCount, 
+		rtvDesc.Format, 
+		srvDescriptorHeap, 
+		srvDescriptorHeap->GetCPUDescriptorHandleForHeapStart(), 
+		srvDescriptorHeap->GetGPUDescriptorHandleForHeapStart());
 }
 void DirectX12::Update() {
 
@@ -38,6 +43,7 @@ void DirectX12::Update() {
 void DirectX12::PreDraw() {
 	DecideCommand();
 	TransitionBarrier();
+	RTV();
 	SetImGuiDescriptorHeap();
 }
 void DirectX12::PostDraw() {
@@ -58,7 +64,7 @@ void DirectX12::End(WindowsAPI* winAPI) {
 //1
 void DirectX12::MakeDXGIFactory() {
 	dxgiFactory = nullptr;
-	hr = CreateDXGIFactory(IID_PPV_ARGS(&dxgiFactory));
+	HRESULT hr = CreateDXGIFactory(IID_PPV_ARGS(&dxgiFactory));
 	assert(SUCCEEDED(hr));
 }
 
@@ -70,7 +76,7 @@ void DirectX12::Adapter() {
 	for (UINT i = 0; dxgiFactory->EnumAdapterByGpuPreference(i, DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(&useAdapter)) != DXGI_ERROR_NOT_FOUND; ++i) {
 		//GetAdapterInfo
 		adapterDesc = {};
-		hr = useAdapter->GetDesc3(&adapterDesc);
+		HRESULT hr = useAdapter->GetDesc3(&adapterDesc);
 		assert(SUCCEEDED(hr));
 		//ソフトウェアでなければok
 		if (!(adapterDesc.Flags & DXGI_ADAPTER_FLAG3_SOFTWARE)) {
@@ -90,7 +96,7 @@ void DirectX12::D3D12Device() {
 	D3D_FEATURE_LEVEL featureLevels[] = { D3D_FEATURE_LEVEL_12_2,D3D_FEATURE_LEVEL_12_1,D3D_FEATURE_LEVEL_12_0 };
 	const char* featureLevelStrings[] = { "12.2","12.1","12.0" };
 	for (size_t i = 0; i < _countof(featureLevels); ++i) {
-		hr = D3D12CreateDevice(useAdapter, featureLevels[i], IID_PPV_ARGS(&device));
+		HRESULT hr = D3D12CreateDevice(useAdapter, featureLevels[i], IID_PPV_ARGS(&device));
 		if (SUCCEEDED(hr)) {
 			LogText(std::format("FeatureLevel:{}\n", featureLevelStrings[i]));
 			break;
@@ -101,7 +107,7 @@ void DirectX12::D3D12Device() {
 void DirectX12::MakeCommandQueue() {
 	commandQueue = nullptr;
 	commandQueueDesc = {};
-	hr = device->CreateCommandQueue(&commandQueueDesc, IID_PPV_ARGS(&commandQueue));
+	HRESULT hr = device->CreateCommandQueue(&commandQueueDesc, IID_PPV_ARGS(&commandQueue));
 	assert(SUCCEEDED(hr));
 }
 
@@ -109,7 +115,7 @@ void DirectX12::MakeCommandQueue() {
 //5
 void DirectX12::MakeCommandList() {
 	commandAllocator = nullptr;
-	hr = device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&commandAllocator));
+	HRESULT hr = device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&commandAllocator));
 	assert(SUCCEEDED(hr));
 
 	commandList = nullptr;
@@ -132,7 +138,7 @@ void DirectX12::MakeSwapChain() {
 	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
 	
 
-	hr = dxgiFactory->CreateSwapChainForHwnd(commandQueue, winAPI_->GetHwnd(), &swapChainDesc, nullptr, nullptr, reinterpret_cast<IDXGISwapChain1**>(&swapChain));
+	HRESULT hr = dxgiFactory->CreateSwapChainForHwnd(commandQueue, winAPI_->GetHwnd(), &swapChainDesc, nullptr, nullptr, reinterpret_cast<IDXGISwapChain1**>(&swapChain));
 	assert(SUCCEEDED(hr));
 }
 
@@ -149,8 +155,15 @@ ID3D12DescriptorHeap* DirectX12::CreateDescriptorHeap(ID3D12Device* device, D3D1
 //7
 void DirectX12::MakeDescriptorHeap() {
 	rtvDescriptorHeap = CreateDescriptorHeap(device, D3D12_DESCRIPTOR_HEAP_TYPE_RTV, 2, false);
+	rtvDescriptorHeapDesc = {};
+	
 	srvDescriptorHeap = CreateDescriptorHeap(device, D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV, 128, false);
 
+	rtvDescriptorHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+	rtvDescriptorHeapDesc.NumDescriptors = 2;
+
+	HRESULT hr = device->CreateDescriptorHeap(&rtvDescriptorHeapDesc, IID_PPV_ARGS(&rtvDescriptorHeap));
+	assert(SUCCEEDED(hr));
 
 	swapChainResources[0] = { nullptr };
 	swapChainResources[1] = { nullptr };
@@ -172,6 +185,7 @@ void DirectX12::MakeRTV() {
 	rtvDesc.ViewDimension = D3D12_RTV_DIMENSION_TEXTURE2D;
 
 	rtvStartHandle = rtvDescriptorHeap->GetCPUDescriptorHandleForHeapStart();
+
 	rtvHandles[0] = rtvStartHandle;
 	device->CreateRenderTargetView(swapChainResources[0], &rtvDesc, rtvHandles[0]);
 	rtvHandles[1].ptr = rtvHandles[0].ptr + device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
@@ -189,6 +203,13 @@ void DirectX12::KickCommand() {
 	commandQueue->ExecuteCommandLists(1, commandLists);
 
 	swapChain->Present(1, 0);
+}
+
+void DirectX12::RTV() {
+	commandList->OMSetRenderTargets(1, &rtvHandles[backBufferIndex], false, nullptr);
+
+	float clearColor[] = { 0.1f,0.25f,0.5f,1.0f };
+	commandList->ClearRenderTargetView(rtvHandles[backBufferIndex], clearColor, 0, nullptr);
 }
 
 void DirectX12::Debug() {
@@ -231,13 +252,6 @@ void DirectX12::TransitionBarrier() {
 	barrier.Transition.StateAfter = D3D12_RESOURCE_STATE_RENDER_TARGET;
 
 	commandList->ResourceBarrier(1, &barrier);
-
-
-	commandList->OMSetRenderTargets(1, &rtvHandles[backBufferIndex], false, nullptr);
-
-	float clearColor[] = { 0.1f,0.25f,0.5f,1.0f };
-	commandList->ClearRenderTargetView(rtvHandles[backBufferIndex], clearColor, 0, nullptr);
-
 }
 
 
@@ -275,7 +289,7 @@ void DirectX12::WaitGPU() {
 		fence->SetEventOnCompletion(fenceValue, fenceEvent);
 		WaitForSingleObject(fenceEvent, INFINITE);
 	}
-	hr = commandAllocator->Reset();
+	HRESULT hr = commandAllocator->Reset();
 	assert(SUCCEEDED(hr));
 	hr = commandList->Reset(commandAllocator, nullptr);
 	assert(SUCCEEDED(hr));
--- DirectX12.h
+++ DirectX12.h
@@ -48,6 +48,7 @@ public:
 	void MakeFenceEvent();
 	void SendSignal();
 	void WaitGPU();
+	void RTV();
 
 	void ReportLiveObject();
 	void AllRelease();
@@ -98,40 +99,6 @@ private:
 	
 	IDXGIDebug1* debug;
 	
-	////InitializeDXC
-	//IDxcUtils* dxcUtils;
-	//IDxcCompiler3* dxcCompiler;
-	//IDxcIncludeHandler* includeHandler;
-	////MakeRootSignature
-	//D3D12_ROOT_SIGNATURE_DESC descriptionRootSignature{};
-	//ID3DBlob* signatureBlob;
-	//ID3DBlob* errorBlob;
-	//ID3D12RootSignature* rootSignature;
-	////SetInputLayout
-	//D3D12_INPUT_ELEMENT_DESC inputElementDescs[1];
-	//D3D12_INPUT_LAYOUT_DESC inputLayoutDesc{};
-	////SetBlendState
-	//D3D12_BLEND_DESC blendDesc{};
-	////SetRasterizerState
-	//D3D12_RASTERIZER_DESC rasterizerDesc{};
-	////ShaderCompile
-	//IDxcBlob* vertexShaderBlob;
-	//IDxcBlob* pixelShaderBlob;
-	////MakePSO
-	//D3D12_GRAPHICS_PIPELINE_STATE_DESC graphicsPipelineStateDesc{};
-	//ID3D12PipelineState* graphicsPipelineState;
-	////MakeVertexResource
-	//D3D12_HEAP_PROPERTIES uploadHeapProperties{};
-	//D3D12_RESOURCE_DESC vertexResourceDesc{};
-	//ID3D12Resource* vertexResource;
-	////MakeVertexBufferView
-	//D3D12_VERTEX_BUFFER_VIEW vertexBufferView{};
-	////DateResource
-	//Vector4* vertexDate;
-	////ViewportScissor
-	//D3D12_VIEWPORT viewport{};
-	//D3D12_RECT scissorRect{};
-
 	const int32_t kClientWidth = 1280;
 	const int32_t kClientHeight = 720;
 };
