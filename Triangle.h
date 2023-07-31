63ae24d3b2c5513a9c11ee09333ef7be2172c627
k022g0101@g.neec.ac.jp <k022g0101@g.neec.ac.jp>
2023-07-31 11:42:54 +0900

    s

--- Triangle.h
+++ Triangle.h
@@ -23,15 +23,22 @@ public:
 private:
 	DirectX12* directX12_;
 
+	Transform transform;
+	Transform cameraTransform;
+
 	HRESULT hr;
 	//MakeVertexResource
 	D3D12_HEAP_PROPERTIES uploadHeapProperties{};
 	D3D12_RESOURCE_DESC vertexResourceDesc{};
 	ID3D12Resource* vertexResource;
+	ID3D12Resource* materialResource;
+	ID3D12Resource* wvpResource;
 	//MakeVertexBufferView
 	D3D12_VERTEX_BUFFER_VIEW vertexBufferView{};
 	//DateResource
 	Vector4* vertexDate;
+	Vector4* materialDate;
+	Matrix4x4* wvpDate;
 	//ViewportScissor
 	D3D12_VIEWPORT viewport{};
 	D3D12_RECT scissorRect{};
