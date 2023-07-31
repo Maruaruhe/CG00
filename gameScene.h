a1b5743c67793c5a25e43b885acae60885cddf07
k022g0101@g.neec.ac.jp <k022g0101@g.neec.ac.jp>
2023-07-31 10:41:04 +0900

    s

--- gameScene.h
+++ gameScene.h
@@ -3,28 +3,26 @@
 #include "DirectX12.h"
 #include "GraphicsRenderer.h"
 #include "Triangle.h"
-#include "ImGuiWND.h"
-#define TRIANGLECOUNT 3
+#define TRIANGLECOUNT 2
 
 class WindowsAPI;
 class GraphicsRenderer;
 class DirectX12;
 
-class MainRoop
+class GameScene
 {
 public:
-	void Initialize(WindowsAPI* winAPI,DirectX12* directX12);
+	void Initialize(WindowsAPI* winAPI, DirectX12* directX12);
 	void Update();
 	void Release();
 	void Draw();
 
-	void First();
+	void PreDraw();
 	void Final();
 
 	void End();
 
 	void VariableInit();
-
 
 private:
 	struct TriangleData
@@ -36,13 +34,7 @@ private:
 
 	TriangleData triangleData[TRIANGLECOUNT];
 
-	WindowsAPI* winAPI_;
 	DirectX12* directX12_ = new DirectX12;
 	GraphicsRenderer* graphicsRenderer_ = new GraphicsRenderer;
 	Triangle* triangle_[TRIANGLECOUNT];
-
-	Matrix4x4 cameraMatrix;
-	Matrix4x4 viewMatrix;
-	Matrix4x4 projectionMatrix;
-	Matrix4x4 worldViewProjectionMatrix;
 };
