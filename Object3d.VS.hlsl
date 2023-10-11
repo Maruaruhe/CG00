6b4b541d0dcd9d60dc31da99cc60298c6958c32d
k022g0101@g.neec.ac.jp <k022g0101@g.neec.ac.jp>
2023-10-11 15:17:46 +0900

    s

--- Object3d.VS.hlsl
+++ Object3d.VS.hlsl
@@ -9,7 +9,7 @@ struct VertexShaderOutput {
 struct  VertexShaderInput {
 	float32_t4 position : POSITION0;
 };
-VertexShaderOutput main() {
+VertexShaderOutput main(VertexShaderInput input) {
 	VertexShaderOutput output;
 	output.position = mul(input.position, gTransformationMatrix.WVP);
 	return output;
