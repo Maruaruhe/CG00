7eefcec31e8299807451c30cca892b567a512355
k022g0101@g.neec.ac.jp <k022g0101@g.neec.ac.jp>
2023-10-31 16:24:14 +0900

    j

--- struct.h
+++ struct.h
@@ -1,1 +1,21 @@
 #pragma once
+#include "Vector4.h"
+#include "Matrix4x4.h"
+
+struct VertexData {
+	Vector4 position;
+	Vector2 texcoord;
+	Vector3 normal;
+};
+
+struct TriangleData
+{
+	VertexData Left_;
+	VertexData Top_;
+	VertexData Right_;
+};
+
+struct Material {
+	Vector4 color;
+	int32_t enableLighting;
+};
