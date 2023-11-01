09b561910465bb3bfe4ec87416a346dbbc68dcd5
k022g0101@g.neec.ac.jp <k022g0101@g.neec.ac.jp>
2023-11-01 15:31:28 +0900

    s

---
+++ Model.cpp
@@ -0,0 +1,163 @@
+#include "Model.h"
+#include <assert.h>
+
+ModelData Model::LoadObjFile(const std::string& directoryPath, const std::string& filename) {
+	ModelData modelData;
+	std::vector<Vector4> positions;
+	std::vector<Vector3> normals;
+	std::vector<Vector2> texcoords;
+	std::string line;
+
+	std::ifstream file(directoryPath + "/" + filename);
+	assert(file.is_open());
+
+	while (std::getline(file, line)) {
+		std::string identifier;
+		std::istringstream s(line);
+		s >> identifier;
+
+		if (identifier == "v") {
+			Vector4 position;
+			s >> position.x >> position.y >> position.z;
+			position.w = 1.0f;
+			positions.push_back(position);
+		}
+		else if (identifier == "vt") {
+			Vector2 texcoord;
+			s >> texcoord.x >> texcoord.y;
+			texcoords.push_back(texcoord);
+		}
+		else if (identifier == "vn") {
+			Vector3 normal;
+			s >> normal.x >> normal.y >> normal.z;
+			normals.push_back(normal);
+		}
+		else if (identifier == "f") {
+			for (int32_t faceVertex = 0; faceVertex < 3; ++faceVertex) {
+				std::string vertexDefinition;
+				s >> vertexDefinition;
+
+				std::istringstream v(vertexDefinition);
+				uint32_t elementIndices[3];
+				for (int32_t element = 0; element < 3; ++element) {
+					std::string index;
+					std::getline(v, index, '/');
+					elementIndices[element] = std::stoi(index);
+				}
+				Vector4 position = positions[elementIndices[0] - 1];
+				Vector2 texcoord = texcoords[elementIndices[1] - 1];
+				Vector3 normal = normals[elementIndices[2] - 1];
+				VertexData vertex = { position,texcoord,normal };
+				modelData.vertices.push_back(vertex);
+			}
+		}
+	}
+	return modelData;
+}
+void Model::InitializePosition() {
+	ModelData modelData = LoadObjFile("resources/plane.obj");
+}
+
+void Model::Initialize(DirectX12* directX12) {
+	directX12_ = directX12;
+	transform = { {1.0f,1.0f,1.0f},{0.0f,0.0f,0.0f},{0.0f,0.0f,0.0f} };
+	cameraTransform = { {1.0f,1.0f,1.0f},{0.0f,0.0f,0.0f},{0.0f,0.0f,-10.0f} };
+	CreateVertexResource();
+	CreateMaterialResource();
+	CreateVertexBufferView();
+	CreateTransformationMatrixResource();
+	CreateDirectionalLightResource();
+	DataResource();
+
+	vertexData = nullptr;
+	vertexResource->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));
+
+	InitializePosition();
+}
+
+void Model::Update(Vector4& color, Transform& transform_, DirectionalLight& direcionalLight) {
+	transform.translate = transform_.translate;
+	transform.rotate = transform_.rotate;
+	transform.scale = transform_.scale;
+	materialData_->uvTransform = MakeIdentity4x4();
+	transformationMatrix->World = MakeAffineMatrix(transform.scale, transform.rotate, transform.translate);
+	Matrix4x4 cameraMatrix = MakeAffineMatrix(cameraTransform.scale, cameraTransform.rotate, cameraTransform.translate);
+	Matrix4x4 viewMatrix = Inverse(cameraMatrix);
+	Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kClientWidth) / float(kClientHeight), 0.1f, 100.0f);
+	Matrix4x4 worldViewProjectionMatrix = Multiply(transformationMatrix->World, Multiply(viewMatrix, projectionMatrix));
+	transformationMatrix->WVP = worldViewProjectionMatrix;
+	materialData_->color = color;
+	directionalLight_->color = direcionalLight.color;
+	directionalLight_->direction = direcionalLight.direction;
+	directionalLight_->intensity = direcionalLight.intensity;
+
+	ImGui::Checkbox("useMonsterBall", &useMonsterBall);
+}
+
+void Model::Draw() {
+	directX12_->GetCommandList()->IASetVertexBuffers(0, 1, &vertexBufferView);	//VBVを設定
+	//形状を設定。PSOに設定しているものとはまた別。同じものを設定すると考えておけばよい
+	directX12_->GetCommandList()->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	directX12_->GetCommandList()->SetGraphicsRootConstantBufferView(0, materialResource_->GetGPUVirtualAddress());
+
+	//directX12_->GetCommandList()->SetGraphicsRootConstantBufferView(0, materialResourceSprite->GetGPUVirtualAddress());
+	//wvp用のCBufferの場所を設定
+	directX12_->GetCommandList()->SetGraphicsRootConstantBufferView(1, wvpResource_->GetGPUVirtualAddress());
+
+	directX12_->GetCommandList()->SetGraphicsRootDescriptorTable(2, useMonsterBall ? directX12_->GetSrvHandleGPU2() : directX12_->GetSrvHandleGPU());
+	directX12_->GetCommandList()->SetGraphicsRootConstantBufferView(3, directionalLightResource->GetGPUVirtualAddress());
+	//描画！　（DrawCall/ドローコール)。3頂点で1つのインスタンス。インスタンスについては今後
+	directX12_->GetCommandList()->DrawInstanced(1536, 1, 0, 0);
+}
+
+void Model::CreateVertexResource() {
+	vertexResource = directX12_->CreateBufferResource(directX12_->GetDevice(), sizeof(VertexData) * 1536);
+}
+
+void Model::CreateVertexBufferView() {
+	vertexBufferView = {};
+	//リソースの先頭のアドレスから使う
+	vertexBufferView.BufferLocation = vertexResource->GetGPUVirtualAddress();
+	//使用するリソースのサイズは頂点３つ分のサイズ
+	vertexBufferView.SizeInBytes = sizeof(VertexData) * 1536;
+	//1頂点当たりのサイズ
+	vertexBufferView.StrideInBytes = sizeof(VertexData);
+}
+
+void Model::CreateMaterialResource() {
+	materialResource_ = directX12_->CreateBufferResource(directX12_->GetDevice(), sizeof(Material));
+	materialData_ = nullptr;
+	materialResource_->Map(0, nullptr, reinterpret_cast<void**>(&materialData_));
+	materialData_->color = Vector4(1.0f, 0.0f, 0.0f, 1.0f);
+	materialData_->enableLighting = true;
+
+	//materialResourceSprite = directX12_->CreateBufferResource(directX12_->GetDevice(), sizeof(Material));
+}
+
+void Model::CreateTransformationMatrixResource() {
+	//WVP用のリソースを作る。Matrix4x4　1つ分のサイズを用意する
+	wvpResource_ = directX12_->CreateBufferResource(directX12_->GetDevice(), sizeof(TransformationMatrix));
+	//データを書き込む
+	transformationMatrix = nullptr;
+	//書き込むためのアドレスを取得
+	wvpResource_->Map(0, nullptr, reinterpret_cast<void**>(&transformationMatrix));
+	//単位行列を書き込んでおく
+	transformationMatrix->WVP = MakeIdentity4x4();
+}
+
+void Model::CreateDirectionalLightResource() {
+	directionalLightResource = directX12_->CreateBufferResource(directX12_->GetDevice(), sizeof(DirectionalLight));
+	directionalLight_ = nullptr;
+	directionalLightResource->Map(0, nullptr, reinterpret_cast<void**>(&directionalLight_));
+}
+
+void Model::DataResource() {
+	//書き込むためのアドレスを取得
+	vertexResource->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));
+}
+
+void Model::Release() {
+	vertexResource->Release();
+	materialResource_->Release();
+	directionalLightResource->Release();
+}
